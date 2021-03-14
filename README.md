0f7f4303b3bc31a83a3bd736b051af8bbcb82db0
Lara Coding <78417151+JoyofCodingLife@users.noreply.github.com>
2021-03-14 14:15:05 +1100

    update 1 - JS shell is created

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # Week3-A-Password-Generator
-Coding Bootcamp - Assignment - Week 3 - Password Generator
+* Coding Bootcamp - Assignment - Week 3 - Password Generator
+
+<br>
+
+## Table of Contents
+* [General Info](#general-info)
+* [Live URL](#live-url)
+* [Walkthrough or What have done so far](#walkthrough)
+* [Webpage Preview](#webpage-preview)
+* [Contributing](#contributing)
+* [Notes](#notes)
+
+<br>
+
+## General Info
+
+<br>
+
+### Live URL
+https://joyofcodinglife.github.io/Week3-A-Password-Generator/
+
+<br>
+
+## Walkthrough
+
+<br>
+
+
+### Webpage Preview
+The following images show the web application's appearance.
+
+
+![Website Preview](./assets/WebsitePreview.png)
+
+<br>
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change, so I learn and understand it better.
+
+Please make sure to update tests as appropriate.
+
+### Notes
+
+---
+© 2021 L Korolyova
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,46 @@
-// Assignment Code
+//Generate Button + Event Listener to prompt questions when button pushed
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Copy button and function to copy the generated password
+var copy = document.querySelector("#copy");
+copy.addEventListener("click", copyPassword);
 
-  passwordText.value = password;
+function copyPassword() {
+    document.getElementById("password").select();
+    document.execCommand("Copy");
+    alert("Password copied to clipboard!");
+}
+
+
+// Various Arrays 
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// Variable Declaration 
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
+
+
+// Questions to configure the password
+function passwordOptions () {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+// Password Generator
+function generatePassword() {
+
+}
+
+// Display password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
