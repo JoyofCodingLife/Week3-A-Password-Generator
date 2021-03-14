cc3000fb59343a96c3b7d294092844ef49bdc41e
Lara Coding <78417151+JoyofCodingLife@users.noreply.github.com>
2021-03-14 17:15:26 +1100

    update 2 - pasword parameters are set

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,12 +29,38 @@ var confirmLowerCase;
 
 // Questions to configure the password
 function passwordOptions () {
+  var confirmLength = (prompt("How many characters would you like your password to contain?"));
 
+    // Loop if answer is outside the parameter of at least 8 characters and no more than 128 characters
+      while (confirmLength <=7 || confirmLength >= 129){
+      alert ("Password length must be between 8-128 characters, please try again");
+      var confirmLength = (prompt("How many characters would you like your password to contain?"));
+      }
+    // Output result
+      alert ("Your passwold will have " + confirmLength + " characters");
+
+  // Confuguration of characters
+  var confirmSpecialCharacter = confirm ("Click OK to confirm if you would like to include SPECIAL characters");
+  var confirmNumericCharacter = confirm ("Click OK to confirm if you would like to include NUMERIC characters");
+  var confirmLowerCase = confirm ("Click OK to confirm if you would like to include LOWERCASE characters");
+  var confirmUpperCase = confirm ("Click OK to confirm if you would like to include UPPERCASE characters");
+      
+    // Loop if all 4 parameters are negative
+      if (!confirmSpecialCharacter && !confirmNumericCharacter && !confirmLowerCase && !confirmUpperCase){
+        alert ("You must choose at least one parameter");
+        var confirmSpecialCharacter = confirm ("Click OK to confirm if you would like to include SPECIAL characters");
+        var confirmNumericCharacter = confirm ("Click OK to confirm if you would like to include NUMERIC characters");
+        var confirmLowerCase = confirm ("Click OK to confirm if you would like to include LOWERCASE characters");
+        var confirmUpperCase = confirm ("Click OK to confirm if you would like to include UPPERCASE characters");
+      }
 }
 
 
 // Password Generator
 function generatePassword() {
+
+  var options = passwordOptions();
+  console.log(options);
 
 }
 
